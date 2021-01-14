11b44060a9dd7b803f8f0071fc51f2d7b7b72495
Laura <laurakcleve@gmail.com>
2021-01-13 20:01:43 -0700

    Formatting

--- client/src/components/SidebarList.jsx
+++ client/src/components/SidebarList.jsx
@@ -7,7 +7,10 @@ const SidebarList = ({ items, selectedName, setSelectedName }) => {
   return (
     <Styled.List>
       {items.map((item) => (
-        <li key={item.id} className={item.name === selectedName ? 'selected' : null}>
+        <li
+          key={item.id}
+          className={item.name === selectedName ? 'selected' : null}
+        >
           <button type="button" onClick={() => setSelectedName(item.name)}>
             {item.name}
           </button>
